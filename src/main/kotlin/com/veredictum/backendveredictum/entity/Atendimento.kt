f470293e98bc76da0124bf23c7e6b1ae40cd3265
Marcio Juliao <marcio.juliao@sptech.school>
2025-05-26 18:09:47 -0300

    deps: migração de BD H2 para MySQL

--- src/main/kotlin/com/veredictum/backendveredictum/entity/Atendimento.kt
+++ src/main/kotlin/com/veredictum/backendveredictum/entity/Atendimento.kt
@@ -8,7 +8,7 @@ import java.time.LocalDateTime
 
 @Entity
 @Table(name = "atendimento")
-data class Atendimento(
+data class  Atendimento(
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     @Id
